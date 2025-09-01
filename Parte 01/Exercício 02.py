5a45011db43b4890a5caab4e18f75fa5a5548b91
Cintia Mendes <cintia.dev.codifica@gmail.com>
2025-09-01 16:10:36 +0000

    Add.SegundaAula

--- Parte 01/Exercício 02.py
+++
@@ -1,7 +0,0 @@
-# Escreva um algoritmo que calcule a tabuada de um número digitado pelo usuário.
-
-print("\n***TABUADA***\n")
-Num = int(input("Digite um número: "))
-for i in range (0,11):
-    resultado = Num * i
-    print (f"{Num} X {i} = {resultado}")
